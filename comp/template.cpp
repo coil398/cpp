6f92dcccdffda9ef6a072a071164aab903076057
coil398 <t.kawase398@gmail.com>
2025-01-08 20:23:42 +0900

    template

--- comp/template.cpp
+++ comp/template.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 #define ll long long
 #define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rep0(i, n) for (int i = 0; i < n; ++i)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+#ifdef DEBUG
+#define dbg(x) cerr << #x << " = " << x << endl
+#else
+#define dbg(x)
+#endif
 
 int main() {
   std::ios::sync_with_stdio(false);
