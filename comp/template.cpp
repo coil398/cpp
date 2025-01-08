2b3a9bb4f66b2633d427d2f22efda584d9acd16f
coil398 <t.kawase398@gmail.com>
2025-01-08 17:25:56 +0900

    io

--- comp/template.cpp
+++ comp/template.cpp
@@ -3,3 +3,10 @@ using namespace std;
 
 #define ll long long
 #define rep(i, a, b) for (int i = a; i < b; ++i)
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  return 0;
+}
